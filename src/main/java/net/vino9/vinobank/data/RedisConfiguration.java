3ec49ce11605a06c157071df9bb58dc785089984
Li Lin <guru.lin@gmail.com>
2023-06-08 10:14:05 +0800

    use redis configuration from application.yaml

--- src/main/java/net/vino9/vinobank/data/RedisConfiguration.java
+++ src/main/java/net/vino9/vinobank/data/RedisConfiguration.java
@@ -1,22 +1,19 @@
 package net.vino9.vinobank.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 class RedisConfiguration {
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
-    @Bean
-    public RedisTemplate<String, CheckingAccount> redisTemplate() {
+    @Autowired
+    public RedisTemplate<String, CheckingAccount> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, CheckingAccount> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
